2d4403df42db22537f85df191cad7d0963a63a70
Minho Kwon <minho.kwon@weareadaptive.com>
2021-06-29 11:04:34 -0400

    styling complete

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
+import {convertTime} from "./convertTime.js";
+import {subtractTime} from "./subtractTime.js";
+
+const btnStartStop = document.getElementById("start_stop");
+const btnResetLap = document.getElementById("reset_lap");
+const timeDisplay = document.getElementById("time");
+const lapTable = document.getElementById("laps");
+
 let status = "stopped";
 let starttime = null; //start time of the current run
 let elapsedtime = 0; //sum total of all the runs
 let previouslap = "00.00.00";
 let displayedtime;
-
-import {convertTime} from "./convertTime.js";
-import {subtractTime} from "./subtractTime.js";
-
-var btnstartstop = document.getElementById("start_stop");
-var btnresetlap = document.getElementById("reset_lap");
-var timedisplay = document.getElementById("time");
-var laptable = document.getElementById("laps");
-
 let token = undefined;
 let newlap;
 let laptime;
@@ -20,50 +19,54 @@ let minlap;
 let maxlap;
 
 
-function refresh() {
-    timedisplay.innerHTML = convertTime(Date.now() - starttime + elapsedtime);
-    token = requestAnimationFrame(refresh);
+function updateTime() {
+    timeDisplay.innerHTML = convertTime(Date.now() - starttime + elapsedtime);
+    token = requestAnimationFrame(updateTime);
 }
 
-btnstartstop.onclick = function startstop() {
+btnStartStop.onclick = function startstop() {
+
     if  (status === "stopped") { //start
         status = "running";
-        btnstartstop.innerHTML = "stop";
-        btnstartstop.style.backgroundColor = "red";
-        btnstartstop.style.color = "rgb(255, 180, 200)";
-        btnresetlap.innerHTML = "lap";
+        btnStartStop.innerHTML = "stop";
+        btnStartStop.style.backgroundColor = "rgb(100,30,30)";
+        btnStartStop.style.color = "rgb(250, 50, 50)";
+        btnResetLap.innerHTML = "lap";
         starttime = Date.now();
-        refresh();
+        updateTime();
     }
+
     else { //stop
         status = "stopped";
-        btnstartstop.innerHTML = "start";
-        btnstartstop.style.backgroundColor = "green";
-        btnstartstop.style.color = "rgb(0, 255, 0)";
-        btnresetlap.innerHTML = "reset";
+        btnStartStop.innerHTML = "start";
+        btnStartStop.style.backgroundColor = "rgb(30,80,30)";
+        btnStartStop.style.color = "rgb(100,200,100)";
+        btnResetLap.innerHTML = "reset";
         cancelAnimationFrame(token);
         elapsedtime += Date.now() - starttime;
     }
 }
  
-btnresetlap.onclick = function resetlap() {
+btnResetLap.onclick = function resetlap() {
+
     if  (status === "stopped") { //reset
         starttime = null;
         elapsedtime = 0;
         previouslap = "00.00.00";
-        timedisplay.innerHTML = "00.00.00";
-        laptable.innerHTML = null;
+        timeDisplay.innerHTML = "00.00.00";
+        lapTable.innerHTML = null;
         maxlap=0;
         minlap=0;
         laparray=[];
-        btnstartstop.style.backgroundColor = "#808080";
-        btnstartstop.style.color = "white";
+        btnStartStop.style.backgroundColor = "#808080";
+        btnStartStop.style.color = "white";
     }
+    
     else { //record lap
         displayedtime = document.getElementById("time").textContent;
         [laptime, newlap] = subtractTime(previouslap, displayedtime);
         previouslap = displayedtime;
-        var row = laptable.insertRow(-1);
+        var row = lapTable.insertRow(-1);
         var cell = row.insertCell(0);
 
         cell.innerHTML = newlap;
@@ -81,20 +84,20 @@ btnresetlap.onclick = function resetlap() {
             else {
                 maxlap = 1;
             }
-            laptable.rows[maxlap].cells[0].classList.add('maxvalue')
-            laptable.rows[minlap].cells[0].classList.add('minvalue')
+            lapTable.rows[maxlap].cells[0].classList.add('maxvalue');
+            lapTable.rows[minlap].cells[0].classList.add('minvalue');
         }
 
         else {
             if (laparray[laparray.length-1] > laparray[maxlap]) {
-                laptable.rows[maxlap].cells[0].classList.remove('maxvalue');
+                lapTable.rows[maxlap].cells[0].classList.remove('maxvalue');
+                lapTable.rows[laparray.length-1].cells[0].classList.add('maxvalue');
                 maxlap = laparray.length - 1;
-                laptable.rows[maxlap].cells[0].classList.add('maxvalue')
             }
             else if (laparray[laparray.length-1] < laparray[minlap]) {
-                laptable.rows[minlap].cells[0].classList.remove('minvalue');
+                lapTable.rows[minlap].cells[0].classList.remove('minvalue');
+                lapTable.rows[laparray.length-1].cells[0].classList.add('minvalue');
                 minlap = laparray.length - 1;
-                laptable.rows[minlap].cells[0].classList.add('minvalue')
             }
         }
     }
